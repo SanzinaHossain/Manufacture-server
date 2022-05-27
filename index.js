e78dbc139af47d2d5fe9bcdb154a354b53ce511d
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-27 20:14:07 +0600

    port

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ const port = process.env.PORT || 5000
 //middle wire for cors
 app.use(cors())
 app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('assignment-12')
+  })
+  
+  app.listen(port, () => {
+    console.log(`assignment-12 ${port}`)
+  })
+  
