e743bc95832d06386d12a19f0aa7c3ee625d0187
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-27 20:08:59 +0600

    final middleware

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const jwt=require('jsonwebtoken');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
+//middle wire for cors
+app.use(cors())
+app.use(express.json())
