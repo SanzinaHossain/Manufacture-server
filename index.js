44e9ea08199aa9d4e54013635b9d543fc4cbae5b
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-27 20:42:11 +0600

    database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors=require('cors')
 const jwt=require('jsonwebtoken');
 require('dotenv').config()
@@ -7,11 +8,30 @@ const port = process.env.PORT || 5000
 //middle wire for cors
 app.use(cors())
 app.use(express.json())
+//database Connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uaiijlk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+          await client.connect();
+          const toolsCollection=client.db('assignment-12').collection('tools');
+          console.log("connected");
+ 
+          
+         
+          
+          
+    }
+    finally{
+ 
+    }
+  }
+  run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('assignment-12')
   })
   
   app.listen(port, () => {
-    console.log(` ${port}`)
+    console.log(` assignment-12 ${port}`)
   })
   
