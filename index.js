9d33ceb6e04eaf1a84899914ea887f5a8c2ea866
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-28 02:08:54 +0600

    admin

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const port = process.env.PORT || 5000
 //middle wire for cors
 app.use(cors())
 app.use(express.json())
+//token verify
+function verifyJWT(req,res,next){
+    const authHeader=req.headers.authorization;
+    if(!authHeader)
+    {
+      return res.status(401).send({message:'Unauthorized access'});
+    }
+    const token=authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+      if(err){
+        return res.status(403).send({message:'Forbidden access'});
+      }
+      req.decoded=decoded;
+      next()
+    })
+ }
 //database Connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uaiijlk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -15,6 +31,7 @@ async function run(){
     try{
           await client.connect();
           const toolsCollection=client.db('assignment-12').collection('tools');
+          const userCollection=client.db('assignment-12').collection('users');
           console.log("connected");
           //get all tools data from database
           app.get('/tools',async(req,res)=>{
@@ -30,6 +47,47 @@ async function run(){
             const tool=await toolsCollection.findOne(query);
             res.send(tool);
         })
+        //user token setup
+        app.put('/user/:email',async(req,res)=>{
+            const email=req.params.email;
+            const user=req.body;
+            const filter={email:email}
+            const options={upsert:true}
+            const updateDoc={
+              $set:user,
+            };
+            const result=await userCollection.updateOne(filter,updateDoc,options);
+            const token=jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+            res.send({result,token});
+          })
+          //get all users
+          app.get('/users',verifyJWT,async(req,res)=>{
+            const query={};
+            const cursor=userCollection.find(query);
+            const users=await cursor.toArray()
+            res.send(users)
+          })
+          //find all admin
+          app.get('/admin/:email',async(req,res)=>{
+            const email=req.params.email;
+            const user=await userCollection.findOne({email:email})
+            const isadmin=user.role==='admin';
+            res.send({admin:isadmin})
+          })
+          app.put('/users/admin/:email',verifyJWT,async(req,res)=>{
+            const email=req.params.email;
+            const requester=req.decoded.email;
+            console.log(requester);
+            const requesterAccount=await userCollection.findOne({email:requester})
+            const filter={email:email}
+              const updateDoc={
+                $set:{role:'admin'},
+            }
+            const result= await userCollection.updateOne(filter,updateDoc);
+            res.send(result);
+            
+          })
+         
  }
     finally{
  
